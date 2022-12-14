77ffc7ca1d9dd7d98ceafc622a0f3ab87ff56cb3
yasmeinelkenany <yasmeinelkenany@gmail.com>
2022-12-14 23:14:19 +0200

    Edit index.html styles.css script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let calcResult = document.getElementById("result");
 let calculations = document.getElementById("calculations");
+let visibaleCalculations = document.getElementById("visibaleCalculations")
 let typedButtons = document.getElementsByClassName("typed-buttons");
 let mathematicSymbols = document.getElementsByClassName("mathematic-symbols")
 let equal = document.getElementById("equal");
@@ -10,17 +11,28 @@ let calculationsArray;
 let result;
 for (const button of typedButtons) {
     button.addEventListener("click", function () {
-        if (button.classList.contains("mathematic-symbols")) {
-            calculations.textContent += ` ${(button.value)} `;
-        }
-        else if (button.classList.contains("dotdot") && button.classList.contains("hundredper")) {
-            calculations.textContent += `${(button.value)}`
-        }
-        else {
-            calculations.textContent += button.value;
+        if (calculations.textContent.length <= 29) {
+            if (button.classList.contains("mathematic-symbols")) {
+                calculations.textContent += ` ${(button.value)} `;
+                visibaleCalculations.textContent += ` ${(button.value)} `;
+            }
+            else if (button.classList.contains("dotdot")) {
+                calculations.textContent += `${(button.value)}`;
+                visibaleCalculations.textContent += `${(button.value)}`;
+            }
+            else if (button.classList.contains("hundredper")) {
+                button.key = " / 100";
+                calculations.textContent += `${button.key}`
+                visibaleCalculations.textContent += "% ";
+            }
+            else {
+                calculations.textContent += button.value;
+                visibaleCalculations.textContent += button.value;
+            }
         }
     });
 }
+
 
 function multiply() {
     let index = calculationsArray.findIndex(element => element == "*")
@@ -81,26 +93,19 @@ function plusMinusOrder() {
     }
 }
 
-function hundredPercent() {
-    let index = calculationsArray.findIndex(element => element == "%")
-    result = Number(calculationsArray[index - 1]) / 100;
-    calculationsArray.splice(calculationsArray.indexOf("%") - 1, 2, result.toString());
-    return result;
-}
-
 function calc() {
     calculationsArray = calculations.textContent.split(' ');
     console.log(calculationsArray)
     if (calculationsArray.includes("")) {
-        return "Error"
+        return "Error";
     }
-    else {
 
-        if (calculationsArray.includes("%")) {
-            hundredPercent();
-            searchForOperator(hundredPercent, "%")
+    else {
+        if (!calculationsArray.includes("*") && !calculationsArray.includes("/") && !calculationsArray.includes("+") && !calculationsArray.includes("-")) {
+            result = calculationsArray[0];
         }
-        if (calculationsArray.includes("*") && calculationsArray.includes("/")) {
+
+        else if (calculationsArray.includes("*") && calculationsArray.includes("/")) {
             if (calculationsArray.indexOf("*") < calculationsArray.indexOf("/")) {
                 multiply();
                 division();
@@ -128,9 +133,8 @@ function calc() {
             }
         }
         else if (!calculationsArray.includes("*") && !calculationsArray.includes("/")) {
-            plusMinusOrder()
+            plusMinusOrder();
         }
-
         return result;
     }
 }
@@ -141,23 +145,32 @@ equal.addEventListener('click', function () {
         calcResult.textContent = "ERROR";
         calcResult.style.fontSize = "2.2rem";
     }
+    else if (+calcResult.textContent !== Math.floor(+calcResult.textContent)) {
+        calcResult.textContent = (+calcResult.textContent).toFixed(2);
+    }
+    if (calcResult.textContent.length > 10) {
+        calcResult.style.fontSize = "1.5rem"
+    }
+    visibaleCalculations.textContent = calcResult.textContent;
+    calculations.textContent = calcResult.textContent
 });
 
 deleteBtn.addEventListener('click', function () {
     calcResult.textContent = "0";
+    visibaleCalculations.textContent = calculations.textContent;
+    visibaleCalculations.textContent = visibaleCalculations.textContent.slice(0, -1)
     calculations.textContent = calculations.textContent.slice(0, -1)
 })
 
 clear.addEventListener('click', function () {
     calculationsArray = calculations.textContent.split(' ');
-    calculations.textContent = "";
+    visibaleCalculations.textContent = "";
     calcResult.textContent = "0";
     calculationsArray.splice(0, calculationsArray.length)
 });
 
 document.addEventListener("keydown", function (event) {
     for (const button of buttons) {
-        if (button.value == event.key) { button.click(); }
-        event.preventDefault()
+        if (button.value == event.key) button.click();
     }
 })
